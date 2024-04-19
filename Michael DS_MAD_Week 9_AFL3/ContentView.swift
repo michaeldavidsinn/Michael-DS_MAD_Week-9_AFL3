d055308c81490244d64f718cd69e08a3c3a12cac
michaeldavidsinn <146686816+michaeldavidsinn@users.noreply.github.com>
2024-04-19 14:05:18 +0700

    Initial Commit

---
+++ Michael DS_MAD_Week 9_AFL3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Michael DS_MAD_Week 9_AFL3
+//
+//  Created by Michael Sin on 19/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
